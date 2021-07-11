ba6879758e733701c575e63277eb2416b0788654
flipper <flipper@mcfly.local>
2021-07-11 11:38:44 -0400

    manually output block 0, to get around bug in bitcoind getblockstats

--- datafetch/getblockbits.sh
+++ datafetch/getblockbits.sh
@@ -1,3 +1,18 @@
+# have to output block 0 manually because of bug in bitcoind
+# https://github.com/bitcoin/bitcoin/issues/19885
+
+# height
+echo 0
+# hash
+echo '"000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"'
+# bits
+echo '"1d00ffff"'
+# difficulty 
+echo 1
+# chainwork
+echo '"0000000000000000000000000000000000000000000000000000000100010001"'
+
+
 for HEIGHT in {1..649616}
 do
     HASH=`bitcoin-cli getblockstats $HEIGHT | jq --monochrome-output '.["blockhash"]' | cut -b2-65`  
--- datafetch/getblockstats.sh
+++ datafetch/getblockstats.sh
@@ -1,3 +1,20 @@
+# have to output block 0 manually because of bug in bitcoind
+# https://github.com/bitcoin/bitcoin/issues/19885
+
+# height
+echo 0
+# hash
+echo '"000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"'
+# subsidy
+echo 5000000000
+# totalfee
+echo 0
+# time
+echo 1231006505
+# mediantime
+echo 1231006505
+
+
 for HEIGHT in {1..649616}
 do
   bitcoin-cli getblockstats $HEIGHT | jq --monochrome-output '.["height","blockhash","subsidy","totalfee","time","mediantime"]'  
